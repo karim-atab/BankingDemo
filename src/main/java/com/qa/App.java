801ba03cb9881e7a7c84fd8e85c1d5502b1b7766
Karim-atab <atabkarim@gmail.com>
2022-10-28 14:32:54 +0100

    Added the main program

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,32 @@
+package com.qa;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        CurrentAccount current = new CurrentAccount(500,10,"Karim A",35);
+        SavingAccount saving = new SavingAccount(500,10,"Karim A",0.1);
+
+        ArrayList<BankAccount> karims_accounts = new ArrayList();
+        karims_accounts.add(current);
+        karims_accounts.add(saving);
+
+        for(BankAccount ba : karims_accounts){
+            System.out.println(ba.toString());
+        }
+       BankAccount saving1 =  karims_accounts.get(1);
+        if(saving1 instanceof SavingAccount){
+            SavingAccount saving2 = (SavingAccount) saving1;
+            saving2.addInterest();
+        }
+        for(BankAccount ba : karims_accounts){
+            System.out.println(ba.toString());
+        }
+    }
+}
